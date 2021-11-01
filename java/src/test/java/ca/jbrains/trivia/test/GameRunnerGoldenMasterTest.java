4fb1eb0ba19fa8ab7390555e97f56dfd3aa656d2
Carmen Lopez <carmen.lopezfolgado@thoughtworks.com>
2021-11-01 19:17:27 +0100

    Duplicating production code in order to create a golden master test

---
+++ java/src/test/java/ca/jbrains/trivia/test/GameRunnerGoldenMasterTest.java
@@ -0,0 +1,37 @@
+
+package ca.jbrains.trivia.test;
+
+import com.adaptionsoft.games.uglytrivia.Game;
+
+import java.util.Random;
+
+
+public class GameRunnerGoldenMasterTest {
+
+	private static boolean notAWinner;
+
+	public static void main(String[] args) {
+		Game aGame = new Game();
+		
+		aGame.add("Chet");
+		aGame.add("Pat");
+		aGame.add("Sue");
+		
+		Random rand = new Random(7777);
+	
+		do {
+			
+			aGame.roll(rand.nextInt(5) + 1);
+			
+			if (rand.nextInt(9) == 7) {
+				notAWinner = aGame.wrongAnswer();
+			} else {
+				notAWinner = aGame.wasCorrectlyAnswered();
+			}
+			
+			
+			
+		} while (notAWinner);
+		
+	}
+}
